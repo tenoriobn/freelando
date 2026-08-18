d4af979df4cf919d34293a9dd697986e47d2472d
Bruno Tenorio <tenoriobn@gmail.com>
2026-08-18 08:55:25 -0300

    feat: :sparkles: Módulo 2: Validações e componentes reativos

--- src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
+++ src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
+  AbstractControl,
+  AbstractControlOptions,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { Router } from '@angular/router';
 import { CadastroService } from '../../shared/services/cadastro.service';
+import {
+  BehaviorSubject,
+  Observable,
+  of,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs';
+import {
+  Cidade,
+  Estado,
+  IbgeService,
+} from '../../shared/services/ibge.service';
+
+export const senhasIguaisValidator: ValidatorFn = (
+  control: AbstractControl,
+): ValidationErrors | null => {
+  const senha = control.get('senha');
+  const confirmaSenha = control.get('confirmaSenha');
+
+  return senha && confirmaSenha && senha.value === confirmaSenha.value
+    ? null
+    : { senhasNaoIguais: true };
+};
 
 @Component({
   selector: 'app-dados-pessoais-form',
@@ -20,51 +48,44 @@ import { CadastroService } from '../../shared/services/cadastro.service';
 export class DadosPessoaisFormComponent implements OnInit {
   dadosPessoaisForm!: FormGroup;
 
-  estados = [
-    { sigla: 'AC', nome: 'Acre' },
-    { sigla: 'AL', nome: 'Alagoas' },
-    { sigla: 'AP', nome: 'Amapá' },
-    { sigla: 'AM', nome: 'Amazonas' },
-    { sigla: 'BA', nome: 'Bahia' },
-    { sigla: 'CE', nome: 'Ceará' },
-    { sigla: 'DF', nome: 'Distrito Federal' },
-    { sigla: 'ES', nome: 'Espírito Santo' },
-    { sigla: 'GO', nome: 'Goiás' },
-    { sigla: 'MA', nome: 'Maranhão' },
-    { sigla: 'MT', nome: 'Mato Grosso' },
-    { sigla: 'MS', nome: 'Mato Grosso do Sul' },
-    { sigla: 'MG', nome: 'Minas Gerais' },
-    { sigla: 'PA', nome: 'Pará' },
-    { sigla: 'PB', nome: 'Paraíba' },
-    { sigla: 'PR', nome: 'Paraná' },
-    { sigla: 'PE', nome: 'Pernambuco' },
-    { sigla: 'PI', nome: 'Piauí' },
-    { sigla: 'RJ', nome: 'Rio de Janeiro' },
-    { sigla: 'RN', nome: 'Rio Grande do Norte' },
-    { sigla: 'RS', nome: 'Rio Grande do Sul' },
-    { sigla: 'RO', nome: 'Rondônia' },
-    { sigla: 'RR', nome: 'Roraima' },
-    { sigla: 'SC', nome: 'Santa Catarina' },
-    { sigla: 'SP', nome: 'São Paulo' },
-    { sigla: 'SE', nome: 'Sergipe' },
-    { sigla: 'TO', nome: 'Tocantins' },
-  ];
+  estado$!: Observable<Estado[]>;
+  cidades$!: Observable<Cidade[]>;
+
+  carregandoCidades$ = new BehaviorSubject<boolean>(false);
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private cadastroService: CadastroService,
+    private ibgeService: IbgeService,
   ) {}
 
   ngOnInit(): void {
-    this.dadosPessoaisForm = this.fb.group({
-      nomeCompleto: ['', Validators.required],
-      estado: ['', Validators.required],
-      cidade: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required, Validators.minLength(6)]],
-      confirmaSenha: ['', Validators.required],
-    });
+    const formOptions: AbstractControlOptions = {
+      validators: senhasIguaisValidator,
+    };
+
+    this.dadosPessoaisForm = this.fb.group(
+      {
+        nomeCompleto: ['', Validators.required],
+        estado: ['', Validators.required],
+        cidade: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirmaSenha: ['', Validators.required],
+      },
+      formOptions,
+    );
+
+    this.carregarEstados();
+    this.configurarListernerEstado();
+  }
+
+  senhasIguaisValidator(group: FormGroup): { [key: string]: any } | null {
+    const senha = group.get('senha')?.value;
+    const confirmaSenha = group.get('confirmaSenha')?.value;
+
+    return senha === confirmaSenha ? null : { senhasNaoIguais: true };
   }
 
   onAnterior(): void {
@@ -81,6 +102,42 @@ export class DadosPessoaisFormComponent implements OnInit {
     }
   }
 
+  private carregarEstados(): void {
+    this.estado$ = this.ibgeService.getEstados();
+  }
+
+  private configurarListernerEstado(): void {
+    const estadoControl = this.dadosPessoaisForm.get('estado');
+
+    if (estadoControl) {
+      this.cidades$ = estadoControl.valueChanges.pipe(
+        startWith(''),
+        tap(() => {
+          this.resetarCidade();
+          this.carregandoCidades$.next(true);
+        }),
+        switchMap((uf) => {
+          if (uf) {
+            return this.ibgeService
+              .getCidadesPorEstado(uf)
+              .pipe(tap(() => this.carregandoCidades$.next(false)));
+          }
+
+          this.carregandoCidades$.next(false);
+          return of([]);
+        }),
+      );
+    }
+  }
+
+  private carregarCidades(uf: string): void {
+    this.carregandoCidades$.next(true);
+    this.cidades$ = this.ibgeService.getCidadesPorEstado(uf);
+    this.cidades$.subscribe(() => {
+      this.carregandoCidades$.next(false);
+    });
+  }
+
   private salvarDadosAtuais() {
     const formValue = this.dadosPessoaisForm.value;
 
@@ -91,5 +148,9 @@ export class DadosPessoaisFormComponent implements OnInit {
       email: formValue.email,
       senha: formValue.senha,
     });
+  }
+
+  private resetarCidade(): void {
+    this.dadosPessoaisForm.get('cidade')?.setValue('');
   }
 }
---
+++ src/app/shared/services/ibge.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
+
+export interface Estado {
+  id: number;
+  sigla: string;
+  nome: string;
+}
+
+export interface Cidade {
+  id: number;
+  nome: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IbgeService {
+  private readonly API_URL =
+    'https://servicodados.ibge.gov.br/api/v1/localidades';
+
+  constructor(private http: HttpClient) {}
+
+  getEstados(): Observable<Estado[]> {
+    return this.http
+      .get<Estado[]>(`${this.API_URL}/estados?orderBy=nome`)
+      .pipe(retry(2));
+  }
+
+  getCidadesPorEstado(uf: string): Observable<Cidade[]> {
+    return this.http
+      .get<Cidade[]>(`${this.API_URL}/estados/${uf}/municipios?orderBy=nome`)
+      .pipe(retry(2));
+  }
+}
